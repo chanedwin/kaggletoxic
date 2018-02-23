b656ada8635f507d5573f6e1cc2d6c8da968640d
Cawinchan <cawin.chan@gmail.com>
2018-02-23 20:28:44 +0800

    hi

--- Gazette.py
+++ Gazette.py
@@ -6,7 +6,6 @@ def bad_word_processor(data):
 
     # pd.set_option('display.max_rows', 4400)
     df = pd.read_csv(data, sep='delimiter', header=None)
-    print(df)
     return df
 
 
@@ -21,6 +20,5 @@ if __name__ == "__main__":
     df = load_data(SAMPLE_DATA_FILE)
     bad_words = bad_word_processor(UNPROCESSED_BAD_WORDS_DATA)
     ap_bad_words = bad_word_processor(ARABIC_PERSIAN_BAD_WORDS)
-    print(ap_bad_words)
-    print(bad_words)
+
 
