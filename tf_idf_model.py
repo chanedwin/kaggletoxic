b84f0fbeb412f5970a5cce3ad8c79a487c849f1d
chanedwin <edwinchan@u.nus.edu>
2018-02-24 14:57:57 +0800

    refactor code

--- tf_idf_model.py
+++ tf_idf_model.py
@@ -1,7 +1,8 @@
-from utils import COMMENT_TEXT_INDEX, TRUTH_LABELS, load_data, dataframe_to_list
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
+
+from utils import COMMENT_TEXT_INDEX, TRUTH_LABELS, load_data, dataframe_to_list
 
 
 def return_tf_idf_sparse_matrix(df):
@@ -23,6 +24,7 @@ def return_tf_idf_sparse_matrix(df):
 
 def build_logistic_regression_model(vector):
     y = df[TRUTH_LABELS]
+    results = {}
     for i, col in enumerate(TRUTH_LABELS):
         lr = LogisticRegression(random_state=i, class_weight='balanced', solver='sag', n_jobs=4, max_iter=10000)
         print("Building {} model for column:{""}".format(i, col))
@@ -32,7 +34,8 @@ def build_logistic_regression_model(vector):
         print("Column:", col)
         print('\nConfusion matrix\n', confusion_matrix(y[col], pred))
         print(classification_report(y[col], pred))
-    return lr
+        results[col] = lr
+    return results
 
 
 if __name__ == "__main__":
@@ -43,4 +46,3 @@ if __name__ == "__main__":
     vector = return_tf_idf_sparse_matrix(df[COMMENT_TEXT_INDEX])
     aggressively_positive_model_report = build_logistic_regression_model(vector)
     print(aggressively_positive_model_report)
-
